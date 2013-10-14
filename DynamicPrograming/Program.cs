0b1abddcb97d61b9d63c80eec83e6572f44b1bbd
ted <lzc850612@gmail.com>
2013-10-14 21:45:21 +0800

    dynamic programming c#

---
+++ DynamicPrograming/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicPrograming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        private static void Start()
+        {
+            dynamic a = new ExpandoObject();
+            a.a = string.Empty;
+
+            
+        }
+    }
+
+    public class ExpandoObject : IDynamicMetaObjectProvider
+    {
+        public DynamicMetaObject GetMetaObject(System.Linq.Expressions.Expression parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
